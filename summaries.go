d66e89e8a2c527a7212936f0b54cc8e4cde843a6
Bjørn Fjukstad <bjorn@cs.uit.no>
2017-01-04 10:10:45 -0500

    add submit button

--- summaries.go
+++ summaries.go
@@ -9,32 +9,44 @@ import (
 	"honnef.co/go/js/dom"
 )
 
-func main() {
+func search(gene string) {
 	d := dom.GetWindow().Document()
 	s := d.GetElementByID("summary")
+
+	doc, err := genenames.GetDoc(gene)
+	if err != nil {
+		s.SetInnerHTML("Could not convert gene symbol to entrez id. " + err.Error())
+		return
+	}
+
+	geneSummary, err := eutils.GeneSummary(doc.EntrezId)
+	if err != nil {
+		s.SetInnerHTML(err.Error())
+		return
+	}
+	summary := geneSummary.Summary
+	s.SetInnerHTML(summary)
+}
+
+func main() {
+	d := dom.GetWindow().Document()
 	p := d.GetElementByID("symbol").(*dom.HTMLInputElement)
+	submit := d.GetElementByID("submit").(*dom.HTMLInputElement)
+
 	p.Focus()
 	p.AddEventListener("keyup", false, func(event dom.Event) {
 		ke := event.(*dom.KeyboardEvent)
+		input := event.Target().(*dom.HTMLInputElement)
+		fmt.Println(input.Value)
+		gene := input.Value
 		if ke.KeyCode == 13 {
-			go func() {
-				input := event.Target().(*dom.HTMLInputElement)
-				fmt.Println(input.Value)
-
-				doc, err := genenames.GetDoc(input.Value)
-				if err != nil {
-					s.SetInnerHTML("Could not convert gene symbol to entrez id. " + err.Error())
-					return
-				}
-
-				geneSummary, err := eutils.GeneSummary(doc.EntrezId)
-				if err != nil {
-					s.SetInnerHTML(err.Error())
-					return
-				}
-				summary := geneSummary.Summary
-				s.SetInnerHTML(summary)
-			}()
+			go search(gene)
 		}
 	})
+
+	submit.AddEventListener("click", false, func(event dom.Event) {
+		gene := p.Value
+		go search(gene)
+	})
+
 }
